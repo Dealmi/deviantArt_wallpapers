eb40f4afc81e9b3d9fe985d60583ecaab970e6b2
Denis Mironov <mironovsan@gmail.com>
2022-10-09 22:20:57 +0300

    initial commit

---
+++ deviantart_wallpaper.py
@@ -0,0 +1,37 @@
+import ctypes 
+import deviantart
+import random
+import requests
+import os
+
+
+
+# Generating a tag for DA request
+if random.randrange (1,3) == 1:
+    mytag="anime"
+else:
+    mytag="animeart"
+    
+# getting data from Deviantart
+
+#create an API object with your client credentials (look up instructions at https://www.deviantart.com/developers/)
+da = deviantart.Api("21379", "3127eadaafabd145c94e636c6d3517fd")
+
+#fetch daily deviations
+# dailydeviations = da.browse_dailydeviations()
+# .. 2days deviations with specific tags
+dailydeviations = da.browse(endpoint='tags', category_path='', seed='', q='', timerange='24hr', tag=mytag, offset=0, limit=50)
+
+# Choosing one random image from the collection
+max=len(dailydeviations["results"])
+result=random.randrange (0,max+1)
+image_url=dailydeviations["results"][result].content["src"]
+
+#Finding out our TMP path
+temp=os.environ['TMP']
+# Downloading picture
+myfile = requests.get(image_url)
+picPath=temp+'\da_image'
+open(picPath, 'wb').write(myfile.content)
+
+ctypes.windll.user32.SystemParametersInfoW(20,0,picPath,0)
